b6592a7c0b87cdc2b11cd12557a52aaafbc40a38
kdaic <develop@kdaic.net>
2021-03-21 01:56:00 +0900

    fix unit_test of test_model_loader

--- test/test_model_loader.cpp
+++ test/test_model_loader.cpp
@@ -27,6 +27,15 @@ TEST(BodyInfoTest, loadModeFile) {
 TEST(ModelLoaderUtilTest, loadModelFile) {
 
   BodyPtr body(new Body());
+  std::string model_url = "./test/model/sample.wrl";
+
+  EXPECT_TRUE(loadBodyFromModelLoader(body, model_url.c_str(), false));
+
+}
+
+TEST(ModelLoaderUtilTest, calcForwardKinematics) {
+
+  BodyPtr body(new Body());
   std::string model_url = "./test/model/PA10/pa10.main.wrl";
 
   EXPECT_TRUE(loadBodyFromModelLoader(body, model_url.c_str(), false));
